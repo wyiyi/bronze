a4c1f2ba8b7699297f50998fb6dc6f83377a40ad
w_yi.neu <w_yi.neu@neusoft.com>
2021-04-06 13:48:25 +0800

    MergeDoc

--- src/main/java/com/amber/demo/util/MergeDoc.java
+++ src/main/java/com/amber/demo/util/MergeDoc.java
@@ -11,14 +11,13 @@ public class MergeDoc {
 
     public static Map<String, String> getFilesDatas(String filePath){
         Map<String, String> files = new HashMap<>();
-        File file = new File(filePath); //需要获取的文件的路径
-        String[] fileNameLists = file.list(); //存储文件名的String数组
-        File[] filePathLists = file.listFiles(); //存储文件路径的String数组
+        File file = new File(filePath);
+        String[] fileNameLists = file.list();
+        File[] filePathLists = file.listFiles();
         for(int i=0;i<filePathLists.length;i++){
             if(filePathLists[i].isFile()){
-                try {//读取指定文件路径下的文件内容
+                try {
                     String fileDatas = readFile(filePathLists[i]);
-                    //把文件名作为key,文件内容为value 存储在map中
                     files.put(fileNameLists[i], fileDatas);
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -29,22 +28,14 @@ public class MergeDoc {
     }
 
     public static String readFile(File path) throws IOException{
-        //创建一个输入流对象
         InputStream is = new FileInputStream(path);
-        //定义一个缓冲区
-        byte[] bytes = new byte[5120000];// 1kb
-        //通过输入流使用read方法读取数据
+        byte[] bytes = new byte[5120000];
         int len = is.read(bytes);
-        //System.out.println("字节数:"+len);
         String str = null;
         while(len!=-1){
-            //把数据转换为字符串
             str = new String(bytes, 0, len);
-            //System.out.println(str);
-            //继续进行读取
             len = is.read(bytes);
         }
-        //释放资源
         is.close();
         return str;
     }
