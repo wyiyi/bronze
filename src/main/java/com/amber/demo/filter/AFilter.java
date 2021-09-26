20656a175ab6a32510bb9210eba007ce88431a21
w_yi.neu <w_yi.neu@neusoft.com>
2021-09-26 10:00:01 +0800

    filter

---
+++ src/main/java/com/amber/demo/filter/AFilter.java
@@ -0,0 +1,35 @@
+package com.amber.demo.filter;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@Order(1)
+@WebFilter(filterName = "aFilter", value = "/*")
+@Component
+public class AFilter implements Filter {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+        LOGGER.info("A Filter初始化,只初始化一次...");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        LOGGER.info("A 拦截前执行...");
+        chain.doFilter(request,response);
+        LOGGER.info("A 拦截后执行...");
+    }
+
+    @Override
+    public void destroy() {
+        LOGGER.info("A Filter销毁...");
+    }
+}
---
+++ src/main/java/com/amber/demo/filter/ComponentFilterOrderConfig.java
@@ -0,0 +1,36 @@
+package com.amber.demo.filter;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ComponentFilterOrderConfig {
+
+    @Bean
+    public FilterRegistrationBean filterBeanOne() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new AFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setOrder(3);
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean filterBeanTwo() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new BFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setOrder(2);
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean filterBeanThree() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new CFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setOrder(1);
+        return filterRegistrationBean;
+    }
+}
