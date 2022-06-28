1f310844fbeddc62de80641c4e5a7298cb528b5b
w_yi.neu <w_yi.neu@neusoft.com>
2022-06-28 17:33:35 +0800

    unuse import

--- src/main/java/com/amber/demo/interceptor/LoginController.java
+++ src/main/java/com/amber/demo/interceptor/LoginController.java
@@ -1,6 +1,5 @@
 package com.amber.demo.interceptor;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
