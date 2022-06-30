c6ce40faf4040675fd992216624a790fbacf968f
w_yi.neu <w_yi.neu@neusoft.com>
2022-06-30 14:25:54 +0800

    拦截器日志输出调整

--- src/main/java/com/amber/demo/interceptor/UserLoginInterceptor.java
+++ src/main/java/com/amber/demo/interceptor/UserLoginInterceptor.java
@@ -2,6 +2,7 @@ package com.amber.demo.interceptor;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.NamedThreadLocal;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,9 +14,12 @@ import javax.servlet.http.HttpServletResponse;
 public class UserLoginInterceptor implements HandlerInterceptor {
 
     private static Logger log = LoggerFactory.getLogger(UserLoginInterceptor.class);
+    private static final ThreadLocal<Long> logTimeThreadLocal = new NamedThreadLocal<>("ThreadLocal StartTime");
 
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        log.info(request.getRequestURI() + " 开始执行");
+        long beginTime = System.currentTimeMillis();
+        logTimeThreadLocal.set(beginTime);
+        log.info(request.getRequestURI() + " 开始执行时间：" + beginTime);
         return true;
     }
 
@@ -23,6 +27,8 @@ public class UserLoginInterceptor implements HandlerInterceptor {
     }
 
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        log.info(request.getRequestURI() + " 执行结束");
+        long beginTime =  logTimeThreadLocal.get();
+        long endTime = System.currentTimeMillis();
+        log.info(request.getRequestURI() + " 执行结束时间：" + endTime + "耗时：" + (endTime - beginTime));
     }
 }
