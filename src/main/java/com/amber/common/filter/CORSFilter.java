f59cfb3f631d7f6f4cd29b6cbdbb608354c341ce
w_yi.neu <w_yi.neu@neusoft.com>
2022-09-28 22:23:34 +0800

    commit modify cors

--- src/main/java/com/amber/common/filter/CORSFilter.java
+++ src/main/java/com/amber/common/filter/CORSFilter.java
@@ -2,16 +2,14 @@ package com.amber.common.filter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 //@WebFilter(filterName = "corsFilter", urlPatterns = {"/*"})
 //@Component
-public class CORSConfig implements Filter {
+public class CORSFilter implements Filter {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AFilter.class);
 
--- src/main/java/com/amber/common/web/UserInfoController.java
+++ src/main/java/com/amber/common/web/UserInfoController.java
@@ -2,18 +2,15 @@ package com.amber.common.web;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/api/amber/userinfo")
-//@CrossOrigin
-public class UserInfo {
+public class UserInfoController {
 
     @GetMapping("/hello")
-    @CrossOrigin
     @ResponseStatus(HttpStatus.OK)
-    public String getUser(HttpServletResponse response){
-        response.setHeader("Access-Control-Allow-Origin", "*");
+//    @CrossOrigin
+    public String getUser(){
         return "hello, amber!";
     }
 }
