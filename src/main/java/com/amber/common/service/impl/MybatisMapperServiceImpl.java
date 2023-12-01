fcba5b07ac1f205e9a8556c65ddb7080b45cd428
w_yi.neu <w_yi.neu@neusoft.com>
2023-12-01 15:03:08 +0800

    commit mybatis-plus IService 和 BaseMapper

--- src/main/java/com/amber/common/service/impl/MybatisMapperServiceImpl.java
+++ src/main/java/com/amber/common/service/impl/MybatisMapperServiceImpl.java
@@ -3,29 +3,31 @@ package com.amber.common.service.impl;
 import com.amber.common.entity.MybatisDemo;
 import com.amber.common.mapper.MybatisMapper;
 import com.amber.common.service.MybatisMapperService;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
 @Service
-public class MybatisMapperServiceImpl implements MybatisMapperService {
-    @Resource
-    MybatisMapper mybatisMapper;
-
-    public void updateById(MybatisDemo mybatisDemo){
-        mybatisMapper.updateById(mybatisDemo);
-    }
-}
-
-
-//public class MybatisMapperServiceImpl extends ServiceImpl<BaseMapper<MybatisDemo>, MybatisDemo> implements MybatisMapperService{
+//public class MybatisMapperServiceImpl implements MybatisMapperService {
 //    @Resource
 //    MybatisMapper mybatisMapper;
 //
-//    public boolean updateById(MybatisDemo mybatisDemo){
+//    public void updateById(MybatisDemo mybatisDemo){
 //        mybatisMapper.updateById(mybatisDemo);
-//
-//        return false;
 //    }
-//
 //}
+
+
+public class MybatisMapperServiceImpl extends ServiceImpl<BaseMapper<MybatisDemo>, MybatisDemo> implements MybatisMapperService{
+    @Resource
+    MybatisMapper mybatisMapper;
+
+    public boolean updateById(MybatisDemo mybatisDemo){
+        mybatisMapper.updateById(mybatisDemo);
+
+        return false;
+    }
+
+}
