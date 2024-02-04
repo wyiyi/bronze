f5593a121974e57e1bbfa35c8907952d1cb9955c
w_yi.neu <w_yi.neu@neusoft.com>
2024-02-04 15:20:00 +0800

    commit part

--- src/test/groovy/com/amber/common/sensitive/mock/entity/UserDO.java
+++ src/test/groovy/com/amber/common/sensitive/mock/entity/UserDO.java
@@ -30,4 +30,62 @@ public class UserDO {
     @TableField(exist = false)
     List<RoleDO> roles;
 
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public String getIdCard() {
+        return this.idCard;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public List<UserHistoryDO> getHistories() {
+        return this.histories;
+    }
+
+    public List<RoleDO> getRoles() {
+        return this.roles;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setHistories(List<UserHistoryDO> histories) {
+        this.histories = histories;
+    }
+
+    public void setRoles(List<RoleDO> roles) {
+        this.roles = roles;
+    }
+
+
 }
